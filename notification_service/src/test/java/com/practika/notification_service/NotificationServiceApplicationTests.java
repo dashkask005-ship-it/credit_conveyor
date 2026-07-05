c25f3dc3858d2bc7861c3afd738e9dcf3e0d126f
britnisc <britnisc@student.21-school.ru>
2026-07-05 12:03:45 +0300

    Создание проекта добавление модулей

---
+++ notification_service/src/test/java/com/practika/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practika.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
