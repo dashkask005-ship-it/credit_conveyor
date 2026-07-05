858f48c6c8110b1fb2bb00941388a90ee8600566
britnisc <britnisc@student.21-school.ru>
2026-07-05 13:02:26 +0300

    Добавление папки dto с java-классами

---
+++ deal_service/src/main/java/com/practika/deal_service/dto/ApplicationRequestDTO.java
@@ -0,0 +1,24 @@
+package com.practika.deal_service.dto;
+
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class ApplicationRequestDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Integer workExperience;
+    private BigDecimal monthlyIncome;
+    private LocalDate birthDate;
+    private String employmentType;
+
+
+    private BigDecimal amount;
+    private Integer term;
+    private String purpose;
+}
---
+++ deal_service/src/main/java/com/practika/deal_service/dto/ApplicationResponseDTO.java
@@ -0,0 +1,24 @@
+package com.practika.deal_service.dto;
+
+
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class ApplicationResponseDTO {
+
+    private Long id;
+    private BigDecimal amount;
+    private Integer term;
+    private String purpose;
+    private String status;
+    private BigDecimal rate;
+    private BigDecimal monthlyPayment;
+    private LocalDateTime createdAt;
+
+    private String clientName;
+    private String clientEmail;
+
+    private String message;
+}
